d50750976a7e969de0320d9730d76373e9aa8eb7
Ryan Workman <rdavid1099@users.noreply.github.com>
2022-09-11 09:07:53 -0600

    Complete first part of tutorial

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -12,22 +12,40 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
-                
             }
-        }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }.padding()
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        Group {
+            MeetingView()
+                .previewInterfaceOrientation(.portrait)
+        }
     }
 }
