78ca54e4c5556a6a8c00623a9ebbc29b642f7922
Ryan Workman <rdavid1099@users.noreply.github.com>
2022-09-12 07:02:40 -0600

    Complete CreatingACardView

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,23 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by ryan on 9/11/22.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] = [
+        DailyScrum(title: "Hangout", attendees: ["Doopy Dooper", "Funion Bunion"], lengthInMinutes: 15, theme: .oxblood),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
